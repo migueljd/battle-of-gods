d19f3027b221fb3d07544200497b222566fa7a55
migueljd <migueljohndoherty@Miguels-MacBook-Pro.local>
2015-04-14 15:54:41 -0300

    Ajustes variados de IA e carta inicial de Zeus

---
+++ Assets/Scripts/Cards/AresCard.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using Cards;
+using TBTK;
+
+namespace Cards
+{
+	public class AresCard : Card
+	{
+		public int AmountDamaged;
+		
+		void Awake(){
+			base.BaseAwake ();
+		}
+		
+		void Start(){
+			magicCard = true;
+		}
+		
+		public override void ActivateMagic(){
+			Debug.Log ("Activated");
+			//Animate somehow
+			List<Unit> enemies = FactionManager.GetAllHostileUnit (GameControl.selectedUnit.factionID);
+			foreach (Unit enemy in enemies) {
+				enemy.ApplyDamage(AmountDamaged);
+			}
+		}
+		
+		public override IEnumerator PlayParticle(Vector3 position){
+			particles.enableEmission = true;
+			particles.transform.position = Camera.main.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, 12));
+			particles.Play ();
+			float timeToEnd = Time.time + particles.duration;
+			while(Time.time < timeToEnd) yield return null;
+			
+			particles.Stop ();
+		}
+		
+	}
+}
+
