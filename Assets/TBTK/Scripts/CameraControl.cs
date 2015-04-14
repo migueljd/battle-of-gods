4d5c0cf330da356bf26d9cbf0de485a7b9b8b43d
migueljd <migueljohndoherty@Miguels-MacBook-Pro.local>
2015-04-14 16:57:44 -0300

    Camera modificada

--- Assets/TBTK/Scripts/CameraControl.cs
+++ Assets/TBTK/Scripts/CameraControl.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using System.Collections;
 
 using TBTK;
-using Cards;
 
 namespace TBTK {
 
@@ -20,6 +19,7 @@ namespace TBTK {
 		#if UNITY_IPHONE || UNITY_ANDROID || UNITY_WP8 || UNITY_BLACKBERRY
 			private Vector3 lastTouchPos=new Vector3(9999, 9999, 9999);
 			private Vector3 moveDir=Vector3.zero;
+			private float moveMagnitude=0;
 			
 			private float touchZoomSpeed;
 		#endif
@@ -92,6 +92,7 @@ namespace TBTK {
 		
 		
 		
+		
 		// Update is called once per frame
 		void Update () {
 			
@@ -100,216 +101,203 @@ namespace TBTK {
 			else deltaT=0.015f;
 
 			
-			if(!CardsHandManager.movingCard){
 			#if UNITY_IPHONE || UNITY_ANDROID || UNITY_WP8 || UNITY_BLACKBERRY
 		
-				if(enableTouchPan){
-					Quaternion camDir=Quaternion.Euler(0, transform.eulerAngles.y, 0);
-					if(Input.touchCount==1){
-						Touch touch=Input.touches[0];
-						if (touch.phase == TouchPhase.Moved){
-							Vector3 deltaPos = touch.position;
-							
-							if(lastTouchPos!=new Vector3(9999, 9999, 9999)){
-								deltaPos=deltaPos-lastTouchPos;
-								moveDir=new Vector3(deltaPos.x, 0, deltaPos.y).normalized*-1;
-							}
+			if(enableTouchPan){
+				Quaternion camDir=Quaternion.Euler(0, transform.eulerAngles.y, 0);
+				if(Input.touchCount==1){
+					Touch touch=Input.touches[0];
+					if (touch.phase == TouchPhase.Moved){
+						Vector3 deltaPos = touch.position;
+						
+						if(lastTouchPos!=new Vector3(9999, 9999, 9999)){
+							deltaPos=deltaPos-lastTouchPos;
+							moveMagnitude=new Vector3(deltaPos.x, 0, deltaPos.y).magnitude*0.1f;
+							moveDir=new Vector3(deltaPos.x, 0, deltaPos.y).normalized*-1;
+						}
+		
+						lastTouchPos=touch.position;
+					}
+				}
+				else lastTouchPos=new Vector3(9999, 9999, 9999);
+
+				Vector3 dir=thisT.InverseTransformDirection(camDir*moveDir)*moveMagnitude;
+				thisT.Translate (dir * panSpeed * deltaT);
+				
+				moveMagnitude=moveMagnitude*(1-deltaT*10);
+			}
 			
-							lastTouchPos=touch.position;
-						}
+			if(enableTouchZoom){
+				if(Input.touchCount==2){
+					Touch touch1 = Input.touches[0];
+					Touch touch2 = Input.touches[1];
+					
+					//~ Vector3 zoomScreenPos=(touch1.position+touch2.position)/2;
+					
+					if(touch1.phase==TouchPhase.Moved && touch1.phase==TouchPhase.Moved){
+						Vector3 dirDelta=(touch1.position-touch1.deltaPosition)-(touch2.position-touch2.deltaPosition);
+						Vector3 dir=touch1.position-touch2.position;
+						float dot=Vector3.Dot(dirDelta.normalized, dir.normalized);
+						
+						if(Mathf.Abs(dot)>0.7f){	
+							touchZoomSpeed=dir.magnitude-dirDelta.magnitude;
+						}	
 					}
-					else lastTouchPos=new Vector3(9999, 9999, 9999);
 					
-					Vector3 dir=thisT.InverseTransformDirection(camDir*moveDir)*1.5f;
-					thisT.Translate (dir * panSpeed * deltaT);
-					
-					moveDir=moveDir*(1-deltaT*5);
 				}
-				
-				if(enableTouchZoom){
-					if(Input.touchCount==2){
-						Touch touch1 = Input.touches[0];
-						Touch touch2 = Input.touches[1];
-						
-						//~ Vector3 zoomScreenPos=(touch1.position+touch2.position)/2;
-						
-						if(touch1.phase==TouchPhase.Moved && touch1.phase==TouchPhase.Moved){
-							Vector3 dirDelta=(touch1.position-touch1.deltaPosition)-(touch2.position-touch2.deltaPosition);
-							Vector3 dir=touch1.position-touch2.position;
-							float dot=Vector3.Dot(dirDelta.normalized, dir.normalized);
-							
-							if(Mathf.Abs(dot)>0.7f){	
-								touchZoomSpeed=dir.magnitude-dirDelta.magnitude;
-							}	
-						}
-						
-					}
-					
-					if(touchZoomSpeed<0){
-						if(Vector3.Distance(camT.position, thisT.position)<maxZoomDistance){
-							camT.Translate(Vector3.forward*Time.deltaTime*zoomSpeed*touchZoomSpeed);
-							currentZoom+=Time.deltaTime*zoomSpeed*touchZoomSpeed;
-						}
-					}
-					else if(touchZoomSpeed>0){
-						if(Vector3.Distance(camT.position, thisT.position)>minZoomDistance){
-							camT.Translate(Vector3.forward*Time.deltaTime*zoomSpeed*touchZoomSpeed);
-							currentZoom+=Time.deltaTime*zoomSpeed*touchZoomSpeed;
-						}
-					}
-					
-					touchZoomSpeed=touchZoomSpeed*(1-Time.deltaTime*5);
-				}
-				
-				if(enableTouchRotate){
-					if(Input.touchCount==2){
-						Touch touch1 = Input.touches[0];
-						Touch touch2 = Input.touches[1];
-						
-						Vector2 delta1=touch1.deltaPosition.normalized;
-						Vector2 delta2=touch2.deltaPosition.normalized;
-						Vector2 delta=(delta1+delta2)/2;
-						
-						float rotX=thisT.rotation.eulerAngles.x-delta.y*rotationSpeed;
-						float rotY=thisT.rotation.eulerAngles.y+delta.x*rotationSpeed;
-						rotX=Mathf.Clamp(rotX, minRotateAngle, maxRotateAngle);
-						
-						//~ Quaternion rot=Quaternion.Euler(delta.y, delta.x, 0);
-						//Debug.Log(rotX+"   "+rotY);
-						thisT.rotation=Quaternion.Euler(rotX, rotY, 0);
-						//thisT.rotation*=rot;
-					}
-				}
-				
-				#endif
-				
-				
-				
-				#if UNITY_EDITOR || !(UNITY_IPHONE && UNITY_ANDROID && UNITY_WP8 && UNITY_BLACKBERRY)
-				
-				//mouse and keyboard
-				if(enableMouseRotate){
-					if(Input.GetMouseButtonDown(1)){
-						initialMousePosX=Input.mousePosition.x;
-						initialMousePosY=Input.mousePosition.y;
-						initialRotX=thisT.eulerAngles.y;
-						initialRotY=thisT.eulerAngles.x;
-					}
 
-					if(Input.GetMouseButton(1)){
-						float deltaX=Input.mousePosition.x-initialMousePosX;
-						float deltaRotX=(.1f*(initialRotX/Screen.width));
-						float rotX=deltaX+deltaRotX;
-						
-						float deltaY=initialMousePosY-Input.mousePosition.y;
-						float deltaRotY=-(.1f*(initialRotY/Screen.height));
-						float rotY=deltaY+deltaRotY;
-						float y=rotY+initialRotY;
-						
-						//limit the rotation
-						if(y>maxRotateAngle){
-							initialRotY-=(rotY+initialRotY)-maxRotateAngle;
-							y=maxRotateAngle;
-						}
-						else if(y<minRotateAngle){
-							initialRotY+=minRotateAngle-(rotY+initialRotY);
-							y=minRotateAngle;
-						}
-						
-						thisT.rotation=Quaternion.Euler(y, rotX+initialRotX, 0);
-					}
-				}	
+				currentZoom+=Time.deltaTime*zoomSpeed*touchZoomSpeed;
 				
-				Quaternion direction=Quaternion.Euler(0, thisT.eulerAngles.y, 0);
-				
-				
-				if(enableKeyPanning){
-					if(Input.GetButton("Horizontal")) {
-						Vector3 dir=transform.InverseTransformDirection(direction*Vector3.right);
-						thisT.Translate (dir * panSpeed * deltaT * Input.GetAxisRaw("Horizontal"));
-					}
+				touchZoomSpeed=touchZoomSpeed*(1-Time.deltaTime*15);
+			}
+			
+			if(enableTouchRotate){
+				if(Input.touchCount==2){
+					Touch touch1 = Input.touches[0];
+					Touch touch2 = Input.touches[1];
+					
+					Vector2 delta1=touch1.deltaPosition.normalized;
+					Vector2 delta2=touch2.deltaPosition.normalized;
+					Vector2 delta=(delta1+delta2)/2;
+					
+					float rotX=thisT.rotation.eulerAngles.x-delta.y*rotationSpeed;
+					float rotY=thisT.rotation.eulerAngles.y+delta.x*rotationSpeed;
+					rotX=Mathf.Clamp(rotX, minRotateAngle, maxRotateAngle);
+					
+					thisT.rotation=Quaternion.Euler(rotX, rotY, 0);
+				}
+			}
+			
+			#endif
+			
+			
+			
+			#if UNITY_EDITOR || !(UNITY_IPHONE && UNITY_ANDROID && UNITY_WP8 && UNITY_BLACKBERRY)
+			
+			//mouse and keyboard
+			if(enableMouseRotate){
+				if(Input.GetMouseButtonDown(1)){
+					initialMousePosX=Input.mousePosition.x;
+					initialMousePosY=Input.mousePosition.y;
+					initialRotX=thisT.eulerAngles.y;
+					initialRotY=thisT.eulerAngles.x;
+				}
 
-					if(Input.GetButton("Vertical")) {
-						Vector3 dir=transform.InverseTransformDirection(direction*Vector3.forward);
-						thisT.Translate (dir * panSpeed * deltaT * Input.GetAxisRaw("Vertical"));
-					}
-				}
-				if(enableMousePanning){
-					Vector3 mousePos=Input.mousePosition;
-					Vector3 dirHor=transform.InverseTransformDirection(direction*Vector3.right);
-					if(mousePos.x<=0) thisT.Translate(dirHor * panSpeed * deltaT * -3);
-					else if(mousePos.x<=mousePanningZoneWidth) thisT.Translate(dirHor * panSpeed * deltaT * -1);
-					else if(mousePos.x>=Screen.width) thisT.Translate(dirHor * panSpeed * deltaT * 3);
-					else if(mousePos.x>Screen.width-mousePanningZoneWidth) thisT.Translate(dirHor * panSpeed * deltaT * 1);
+				if(Input.GetMouseButton(1)){
+					float deltaX=Input.mousePosition.x-initialMousePosX;
+					float deltaRotX=(.1f*(initialRotX/Screen.width));
+					float rotX=deltaX+deltaRotX;
 					
-					Vector3 dirVer=transform.InverseTransformDirection(direction*Vector3.forward);
-					if(mousePos.y<=0) thisT.Translate(dirVer * panSpeed * deltaT * -3);
-					else if(mousePos.y<=mousePanningZoneWidth) thisT.Translate(dirVer * panSpeed * deltaT * -1);
-					else if(mousePos.y>=Screen.height) thisT.Translate(dirVer * panSpeed * deltaT * 3);
-					else if(mousePos.y>Screen.height-mousePanningZoneWidth) thisT.Translate(dirVer * panSpeed * deltaT * 1);
-				}
-				
-				
-				if(enableMouseZoom){
-					float zoomInput=Input.GetAxis("Mouse ScrollWheel");
-					if(zoomInput!=0){
-						currentZoom+=zoomSpeed*zoomInput;
-						currentZoom=Mathf.Clamp(currentZoom, -maxZoomDistance, -minZoomDistance);
-					}
-				}
-				
-				
-				#endif
-				
-				
-				if(avoidClipping){
-					Vector3 aPos=thisT.TransformPoint(new Vector3(0, 0, currentZoom));
-					Vector3 dirC=aPos-thisT.position;
-					float dist=Vector3.Distance(aPos, thisT.position);
-					RaycastHit hit;
-					obstacle=Physics.Raycast (thisT.position, dirC, out hit, dist);
+					float deltaY=initialMousePosY-Input.mousePosition.y;
+					float deltaRotY=-(.1f*(initialRotY/Screen.height));
+					float rotY=deltaY+deltaRotY;
+					float y=rotY+initialRotY;
 					
-					if(!obstacle){
-						float camZ=Mathf.Lerp(camT.localPosition.z, currentZoom, Time.deltaTime*4);
-						camT.localPosition=new Vector3(camT.localPosition.x, camT.localPosition.y, camZ);
+					//limit the rotation
+					if(y>maxRotateAngle){
+						initialRotY-=(rotY+initialRotY)-maxRotateAngle;
+						y=maxRotateAngle;
 					}
-					else{
-						dist=Vector3.Distance(hit.point, thisT.position)*0.85f;
-						float camZ=Mathf.Lerp(camT.localPosition.z, -dist, Time.deltaTime*50);
-						camT.localPosition=new Vector3(camT.localPosition.x, camT.localPosition.y, camZ);
+					else if(y<minRotateAngle){
+						initialRotY+=minRotateAngle-(rotY+initialRotY);
+						y=minRotateAngle;
 					}
+					
+					thisT.rotation=Quaternion.Euler(y, rotX+initialRotX, 0);
 				}
-				else{
+			}	
+			
+			Quaternion direction=Quaternion.Euler(0, thisT.eulerAngles.y, 0);
+			
+			
+			if(enableKeyPanning){
+				if(Input.GetButton("Horizontal")) {
+					Vector3 dir=transform.InverseTransformDirection(direction*Vector3.right);
+					thisT.Translate (dir * panSpeed * deltaT * Input.GetAxisRaw("Horizontal"));
+				}
+
+				if(Input.GetButton("Vertical")) {
+					Vector3 dir=transform.InverseTransformDirection(direction*Vector3.forward);
+					thisT.Translate (dir * panSpeed * deltaT * Input.GetAxisRaw("Vertical"));
+				}
+			}
+			if(enableMousePanning){
+				Vector3 mousePos=Input.mousePosition;
+				Vector3 dirHor=transform.InverseTransformDirection(direction*Vector3.right);
+				if(mousePos.x<=0) thisT.Translate(dirHor * panSpeed * deltaT * -3);
+				else if(mousePos.x<=mousePanningZoneWidth) thisT.Translate(dirHor * panSpeed * deltaT * -1);
+				else if(mousePos.x>=Screen.width) thisT.Translate(dirHor * panSpeed * deltaT * 3);
+				else if(mousePos.x>Screen.width-mousePanningZoneWidth) thisT.Translate(dirHor * panSpeed * deltaT * 1);
+				
+				Vector3 dirVer=transform.InverseTransformDirection(direction*Vector3.forward);
+				if(mousePos.y<=0) thisT.Translate(dirVer * panSpeed * deltaT * -3);
+				else if(mousePos.y<=mousePanningZoneWidth) thisT.Translate(dirVer * panSpeed * deltaT * -1);
+				else if(mousePos.y>=Screen.height) thisT.Translate(dirVer * panSpeed * deltaT * 3);
+				else if(mousePos.y>Screen.height-mousePanningZoneWidth) thisT.Translate(dirVer * panSpeed * deltaT * 1);
+			}
+			
+			
+			if(enableMouseZoom){
+				float zoomInput=Input.GetAxis("Mouse ScrollWheel");
+				if(zoomInput!=0){
+					currentZoom+=zoomSpeed*zoomInput;
+				}
+			}
+			
+			
+			#endif
+			
+			
+			if(avoidClipping){
+				currentZoom=Mathf.Clamp (currentZoom, minZoomDistance, maxZoomDistance);
+				Vector3 aPos=thisT.TransformPoint(new Vector3(0, 0, currentZoom));
+				Vector3 dirC=aPos-thisT.position;
+				float dist=Vector3.Distance(aPos, thisT.position);
+				RaycastHit hit;
+				obstacle=Physics.Raycast (thisT.position, dirC, out hit, dist);
+				
+				if(!obstacle){
 					float camZ=Mathf.Lerp(camT.localPosition.z, currentZoom, Time.deltaTime*4);
 					camT.localPosition=new Vector3(camT.localPosition.x, camT.localPosition.y, camZ);
 				}
-				
-				
-				float x=Mathf.Clamp(thisT.position.x, minPosX, maxPosX);
-				float z=Mathf.Clamp(thisT.position.z, minPosZ, maxPosZ);
-				
-				thisT.position=new Vector3(x, thisT.position.y, z);
+				else{
+					dist=Vector3.Distance(hit.point, thisT.position)*0.85f;
+					float camZ=Mathf.Lerp(camT.localPosition.z, -dist, Time.deltaTime*50);
+					camT.localPosition=new Vector3(camT.localPosition.x, camT.localPosition.y, camZ);
+				}
 			}
+			else{
+				currentZoom=Mathf.Clamp(currentZoom, -maxZoomDistance, -minZoomDistance);
+				float camZ=Mathf.Lerp(camT.localPosition.z, currentZoom, Time.deltaTime*4);
+				camT.localPosition=new Vector3(camT.localPosition.x, camT.localPosition.y, camZ);
+			}
+			
+			
+			float x=Mathf.Clamp(thisT.position.x, minPosX, maxPosX);
+			float z=Mathf.Clamp(thisT.position.z, minPosZ, maxPosZ);
+			
+			thisT.position=new Vector3(x, thisT.position.y, z);
+			
 		}
 
 		public static void IncreaseMaxCameraMovement(bool increaseX, bool increaseZ){
 			instance._IncreaseMaxCameraMovement (increaseX, increaseZ);
 		}
-
+		
 		public static void DecreaseMinCameraMovement(bool decreaseX, bool decreaseZ){
 			instance._DecreaseMinCameraMovement (decreaseX, decreaseZ);
 		}
-
+		
 		private void _IncreaseMaxCameraMovement(bool increaseX, bool increaseZ){
 			this.maxPosX += increaseX ? this.xChange : 0;
 			this.maxPosZ += increaseZ ? this.zChange : 0;
 		}
-
+		
 		private void _DecreaseMinCameraMovement(bool decreaseX, bool decreaseZ){
 			this.minPosX -= decreaseX ? this.xChange : 0;
 			this.minPosZ -= decreaseZ ? this.zChange : 0;
 		}
-
+		
 		public bool avoidClipping=false;
 		private bool obstacle=false;
 		
