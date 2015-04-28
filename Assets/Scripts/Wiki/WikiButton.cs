be3ffc5ab9b3f0746ee72119e526a71be00b9063
migueljd <migueljohndoherty@Miguels-MacBook-Pro.local>
2015-04-28 15:02:54 -0300

    Wiki implementada

---
+++ Assets/Scripts/Wiki/WikiButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+
+public class WikiButton : MonoBehaviour {
+
+	public Sprite textImage;
+
+
+	public void ButtonPress(){
+		WikiManager.ButtonClicked (this.transform);
+	}
+}
---
+++ Assets/Scripts/Wiki/WikiManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class WikiManager : MonoBehaviour {
+
+	private static WikiManager instance;
+
+
+	public Transform buttonInUse;
+
+	public Font normalFont;
+	public Font selectedFont;
+	public Image wikiInfo;
+
+	void Awake(){
+		if (instance == null) {
+			Debug.Log ("wtf?");
+			instance = this;
+		} else {
+			Debug.Log ("wtf 2?");
+			Destroy(this.gameObject);
+		}
+	
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public static void ButtonClicked(Transform button){
+		Debug.Log (instance);
+		instance._ButtonClicked (button);
+	}
+
+	private void _ButtonClicked(Transform button){
+		if (buttonInUse != null) {
+			buttonInUse.GetComponent<Image>().enabled = false;
+			buttonInUse.GetChild(0).GetComponent<Text>().font = normalFont;
+		}
+		
+		buttonInUse = button;
+		button.GetComponent<Image> ().enabled = true;
+		buttonInUse.GetChild(0).GetComponent<Text>().font = selectedFont;
+		wikiInfo.sprite = button.GetComponent<WikiButton> ().textImage;
+
+
+	}
+
+
+}
