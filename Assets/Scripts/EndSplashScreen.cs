9cb402483f00830c5731e3d5e65b929ce7f912b8
Harry Florencio <harry.costta@gmail.com>
2015-04-28 21:23:06 -0300

    Cena Credits

---
+++ Assets/Scripts/EndSplashScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndSplashScreen : MonoBehaviour {
+
+
+	public void EndScreen () {
+	
+		StartCoroutine (EndSplashScreenRoutine());
+	}
+
+	IEnumerator EndSplashScreenRoutine (){
+		yield return new WaitForSeconds (1);
+		Application.LoadLevel (1);
+	}
+}
