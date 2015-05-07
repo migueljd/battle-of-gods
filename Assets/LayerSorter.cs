90c9254095212d8f94603889d90588b225cdf841
migueljd <migueljohndoherty@Miguels-MacBook-Pro.local>
2015-05-07 16:51:39 -0300

    Ajustes de CORAÇAUM aparecendo na frente da UI

---
+++ Assets/LayerSorter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LayerSorter : MonoBehaviour {
+
+	public int sortingLayerOrder = 1;
+
+	// Use this for initialization
+	void Start () {
+		for(int a = 0; a < transform.childCount; a++)
+		{
+			transform.GetChild(a).GetComponent<MeshRenderer>().sortingOrder = sortingLayerOrder;
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
