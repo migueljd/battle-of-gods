066ad84d2768caec513d76d9166dac7d5a8d7590
Harry Florencio <harry.costta@gmail.com>
2015-05-12 12:05:44 -0300

    menos Tiles por lvl

--- Assets/Scripts/DB/Levels_DB.cs
+++ Assets/Scripts/DB/Levels_DB.cs
@@ -81,21 +81,13 @@ public class Levels_DB : MonoBehaviour {
 	public static int GetTileCount(int lvl){
 		switch (lvl) {
 		case 1:
-			return 6;
+			return 7;
 		case 2:
 			return 8;
 		case 3:
 			return 9;
-		case 4:
-			return 10;
-		case 5:
-			return 11;
-		case 6:
-			return 12;
-		case 7:
-			return 13;
 		default:
-			return 14;
+			return 10;
 		}
 	}
 
